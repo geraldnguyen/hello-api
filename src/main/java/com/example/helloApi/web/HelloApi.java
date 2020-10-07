47048dc837236be226fe2b0e8a1b7bbf44e69933
geraldnguyen <thehuy.nguyen@mastercard.com>
2020-10-07 23:40:17 +0800

    Add logging

--- src/main/java/com/example/helloApi/web/HelloApi.java
+++ src/main/java/com/example/helloApi/web/HelloApi.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j2;
+
 @RequestMapping("/hello")
 @RestController
+@Log4j2
 public class HelloApi {
 
   @Value("${hello.default:World}")
@@ -20,6 +23,8 @@ public class HelloApi {
 
   @GetMapping("/{name}")
   public String helloName(@PathVariable("name") String name) {
-    return String.format("Hello %s", name);
+    String response = String.format("Hello %s", name);
+    log.info(response);
+    return response;
   }
 }
